7f38c2ddcd7b4060b34a915144fa166a0dcdda5f
kgzet <164067705+kgzet@users.noreply.github.com>
2024-06-16 11:13:29 +0200

    adding comments, cleaning the code

--- ui.R
+++ ui.R
@@ -4,13 +4,13 @@
 # install.packages("reticulate")
 # library(reticulate)
 
+library(shiny)
+library(plotly)
+
 #
 # This is the user-interface definition of a Shiny web application. You can
 # run the application by clicking 'Run App' above.
 #
-
-library(shiny)
-library(plotly)
 
 # Define UI for application that draws a bar plot
 fluidPage(
@@ -22,11 +22,17 @@ fluidPage(
     fluidRow(
       
       sidebarPanel(
-        selectInput("select_aa", "Select an amino acid:", choices=""),
-        # selectInput(inputId = "select_aa", label = "Select an amino acid:", choices = choices),
+        
+        selectInput("select_aa", "Select an amino acid:", choices=""),  # updateSelectInput is more memory-saving
+        # selectInput(inputId = "select_aa", label = "Select an amino acid:", choices = choices), # standard selectInput
+        
+        # the radio button to choose kind of data: number of occurrences or percentage
+        radioButtons(inputId = "radio_data", label = "Choose data to show:", choices = c("numbers", "percentage")),
+        
+        # some radio buttons to choose the decoration of the plot
         radioButtons(inputId = "radio_color", label = "Choose color:", choices = c("grey", "darkred", "violet", "lightblue")),
         radioButtons(inputId = "radio_border", label = "Choose border:", choices = c("black", "sienna", "plum4", "lightpink")),
-        radioButtons(inputId = "radio_data", label = "Choose data to show:", choices = c("numbers", "percentage")),
+        
       ),
       
       # Show a plot of the generated distribution
