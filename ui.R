e6f2f70f633be17cc2791ad1f1aeb038673d2b83
kgzet <164067705+kgzet@users.noreply.github.com>
2024-06-16 10:36:17 +0200

    adding select, radio buttons

--- ui.R
+++ ui.R
@@ -4,38 +4,33 @@
 # install.packages("reticulate")
 # library(reticulate)
 
-
-
 #
 # This is the user-interface definition of a Shiny web application. You can
 # run the application by clicking 'Run App' above.
 #
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
 
 library(shiny)
+library(plotly)
 
-# Define UI for application that draws a histogram
+# Define UI for application that draws a bar plot
 fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Codon bias on the example of FeLV"),
 
     # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+    fluidRow(
+      sidebarPanel(
+        # selectInput("select_aa", "Select an amino acid:", choices=""),
+        selectInput(inputId = "select_aa", label = "Select an amino acid:", choices = choices),
+        radioButtons(inputId = "radio_color", label = "Choose color:", choices = c("grey", "darkred", "violet", "lightblue")),
+        radioButtons(inputId = "radio_border", label = "Choose border:", choices = c("black", "sienna", "plum4", "lightpink")),
+        radioButtons(inputId = "radio_data", label = "Choose data to show:", choices = c("numbers", "percentage")),
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+        plotlyOutput("distPlot")
+      )
     )
 )
